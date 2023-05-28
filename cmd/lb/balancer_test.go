0390f062f2bf616a3b5d89ec2250615da1fb791b
Artem Kushch <artemkusch2@gmail.com>
2023-05-29 01:42:59 +0300

    Add first unittest to test the balancing algorithm

--- cmd/lb/balancer_test.go
+++ cmd/lb/balancer_test.go
@@ -1,7 +1,29 @@
 package main
 
-import "testing"
+import (
+	"testing"
 
-func TestBalancer(t *testing.T) {
-	// TODO: Реалізуйте юніт-тест для балансувальникка.
+	. "gopkg.in/check.v1"
+)
+
+// Create a test suite
+type MinServerIndexSuite struct{}
+
+var _ = Suite(&MinServerIndexSuite{})
+
+// Test case for the minServerIndex function
+func (s *MinServerIndexSuite) TestMinServerIndex(c *C) {
+	// Set up the test data
+	serversPool[0].ConnCnt = 10
+	serversPool[1].ConnCnt = 5
+	serversPool[2].ConnCnt = 7
+
+	// Call the minServerIndex function
+	minIndex := minServerIndex()
+
+	// Check the result
+	c.Assert(minIndex, Equals, 1)
 }
+
+// Run all the test suites
+func Test(t *testing.T) { TestingT(t) }
