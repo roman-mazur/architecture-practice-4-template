d2c23872fcdb3f19ad9e9d569d71c9831ad0ee1f
Artem Kushch <artemkusch2@gmail.com>
2023-05-29 18:57:55 +0300

    Delete unittest due to change of balancer implementation

--- cmd/lb/balancer_test.go
+++ cmd/lb/balancer_test.go
@@ -6,24 +6,8 @@ import (
 	. "gopkg.in/check.v1"
 )
 
-// Create a test suite
-type MinServerIndexSuite struct{}
+type TestBalancer struct{}
 
-var _ = Suite(&MinServerIndexSuite{})
+var _ = Suite(&TestBalancer{})
 
-// Test case for the minServerIndex function
-func (s *MinServerIndexSuite) TestMinServerIndex(c *C) {
-	// Set up the test data
-	serversPool[0].ConnCnt = 10
-	serversPool[1].ConnCnt = 5
-	serversPool[2].ConnCnt = 7
-
-	// Call the minServerIndex function
-	minIndex := minServerIndex()
-
-	// Check the result
-	c.Assert(minIndex, Equals, 1)
-}
-
-// Run all the test suites
 func Test(t *testing.T) { TestingT(t) }
