7018218f116c9759bb699deb87a82501360eef31
Kirillbiliashov <Kirillbeliashov14@gmail.com>
2023-05-30 16:11:46 +0300

    Unhealthy server test was added

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -13,6 +13,7 @@ import (
 )
 
 var port = flag.Int("port", 8080, "server port")
+var test = flag.Bool("test", false, "if yes, server health status can be modified for test purpose")
 
 const confResponseDelaySec = "CONF_RESPONSE_DELAY_SEC"
 const confHealthFailure = "CONF_HEALTH_FAILURE"
@@ -20,9 +21,22 @@ const confHealthFailure = "CONF_HEALTH_FAILURE"
 func main() {
 	h := new(http.ServeMux)
 
+	healthCorrupt := false
+	if *test {
+		h.HandleFunc("/health-corrupt", func(rw http.ResponseWriter, r *http.Request) {
+			if r.Method != "POST" {
+				http.Error(rw, "Method not allowed", http.StatusMethodNotAllowed)
+				return
+			}
+			healthCorrupt = true
+			rw.WriteHeader(http.StatusOK)
+			_, _ = rw.Write([]byte("OK"))
+		})
+	}
+
 	h.HandleFunc("/health", func(rw http.ResponseWriter, r *http.Request) {
 		rw.Header().Set("content-type", "text/plain")
-		if failConfig := os.Getenv(confHealthFailure); failConfig == "true" {
+		if failConfig := os.Getenv(confHealthFailure); failConfig == "true" || healthCorrupt {
 			rw.WriteHeader(http.StatusInternalServerError)
 			_, _ = rw.Write([]byte("FAILURE"))
 		} else {
--- integration/balancer_test.go
+++ integration/balancer_test.go
@@ -1,6 +1,7 @@
 package integration
 
 import (
+	"bytes"
 	"fmt"
 	. "gopkg.in/check.v1"
 	"net/http"
@@ -20,24 +21,32 @@ type IntegrationTestSuite struct{}
 var _ = Suite(&IntegrationTestSuite{})
 
 func TestBalancer(t *testing.T) {
-
 	TestingT(t)
-
 }
 
 func (s *IntegrationTestSuite) TestGetRequest(c *C) {
 	if _, exists := os.LookupEnv("INTEGRATION_TEST"); !exists {
 		c.Skip("Integration test is not enabled")
 	}
-
+	var resp *http.Response
+	var err error
 	for i := 0; i < 3; i++ {
-		resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
+		resp, err = client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
 		if err != nil {
 			c.Error(err)
 		}
 		c.Check(resp.Header.Get("lb-from"), Equals, "server"+string(rune(i+1))+":8080")
 	}
-
+	resp, err = client.Post("http://server1:8080/corrupt-health", "", bytes.NewBuffer([]byte{}))
+	if err != nil || resp.StatusCode != http.StatusOK {
+		c.Error(err)
+	}
+	time.Sleep(10 * time.Second)
+	resp, err = client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
+	if err != nil {
+		c.Error(err)
+	}
+	c.Check(resp.Header.Get("lb-from"), Equals, "server2:8080")
 }
 
 func BenchmarkBalancer(b *testing.B) {
