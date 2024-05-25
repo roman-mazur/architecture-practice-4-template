5b08acbd5532d97de02cca3be2d21597c589b3f1
Alexandrus Mechanicus <sologub.sasha2001@gmail.com>
2024-05-25 18:21:46 +0300

    Add integration tests for balancer

--- integration/balancer_test.go
+++ integration/balancer_test.go
@@ -10,23 +10,50 @@ import (
 
 const baseAddress = "http://balancer:8090"
 
-var client = http.Client{
+var client = &http.Client{
 	Timeout: 3 * time.Second,
 }
 
 func TestBalancer(t *testing.T) {
-	if _, exists := os.LookupEnv("INTEGRATION_TEST"); !exists {
+	if !isIntegrationTestEnabled() {
 		t.Skip("Integration test is not enabled")
 	}
 
-	// TODO: Реалізуйте інтеграційний тест для балансувальникка.
+	if !isBalancerAvailable() {
+		t.Skip("Balancer is not available")
+	}
+
 	resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
 	if err != nil {
-		t.Error(err)
+		t.Fatalf("Failed to get response from balancer: %v", err)
 	}
-	t.Logf("response from [%s]", resp.Header.Get("lb-from"))
+	defer resp.Body.Close()
+
+	t.Logf("Response from balancer: [%s]", resp.Header.Get("lb-from"))
+}
+
+func isIntegrationTestEnabled() bool {
+	_, exists := os.LookupEnv("INTEGRATION_TEST")
+	return exists
+}
+
+func isBalancerAvailable() bool {
+	resp, err := client.Get(baseAddress)
+	if err != nil {
+		return false
+	}
+	defer resp.Body.Close()
+	return resp.StatusCode == http.StatusOK
 }
 
 func BenchmarkBalancer(b *testing.B) {
-	// TODO: Реалізуйте інтеграційний бенчмарк для балансувальникка.
+	for i := 0; i < b.N; i++ {
+		resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
+		if err != nil {
+			b.Errorf("Request failed: %v", err)
+		}
+		if resp != nil {
+			resp.Body.Close()
+		}
+	}
 }
