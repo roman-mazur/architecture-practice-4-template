d846b1c318ecf5ff34f00c01cdfcecd82d6fad5a
Illia <kroshka.illya@lll.kpi.ua>
2024-05-27 23:18:33 +0300

    test: implement integration tests and CI

--- integration/balancer_test.go
+++ integration/balancer_test.go
@@ -2,11 +2,18 @@ package integration
 
 import (
 	"fmt"
+	. "gopkg.in/check.v1"
 	"net/http"
 	"os"
 	"testing"
 	"time"
 )
+
+func Test(t *testing.T) { TestingT(t) }
+
+type IntegrationSuite struct{}
+
+var _ = Suite(&IntegrationSuite{})
 
 const baseAddress = "http://balancer:8090"
 
@@ -14,19 +21,53 @@ var client = http.Client{
 	Timeout: 3 * time.Second,
 }
 
-func TestBalancer(t *testing.T) {
+func (s *IntegrationSuite) TestBalancer(c *C) {
 	if _, exists := os.LookupEnv("INTEGRATION_TEST"); !exists {
-		t.Skip("Integration test is not enabled")
+		c.Skip("Integration test is not enabled")
 	}
 
-	// TODO: Реалізуйте інтеграційний тест для балансувальникка.
-	resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
+	server1, err := client.Get(fmt.Sprintf("%s/check", baseAddress))
 	if err != nil {
-		t.Error(err)
+		c.Error(err)
 	}
-	t.Logf("response from [%s]", resp.Header.Get("lb-from"))
+
+	server1Header := server1.Header.Get("lb-from")
+	c.Check(server1Header, Equals, "server1:8080")
+
+	server2, err := client.Get(fmt.Sprintf("%s/check4", baseAddress))
+	if err != nil {
+		c.Error(err)
+	}
+
+	server2Header := server2.Header.Get("lb-from")
+	c.Check(server2Header, Equals, "server2:8080")
+
+	server3, err := client.Get(fmt.Sprintf("%s/check2", baseAddress))
+	if err != nil {
+		c.Error(err)
+	}
+
+	server3Header := server3.Header.Get("lb-from")
+	c.Check(server3Header, Equals, "server3:8080")
+
+	server1Repeat, err := client.Get(fmt.Sprintf("%s/check", baseAddress))
+	if err != nil {
+		c.Error(err)
+	}
+
+	server1RepeatHeader := server1Repeat.Header.Get("lb-from")
+	c.Check(server1RepeatHeader, Equals, server1Header)
 }
 
-func BenchmarkBalancer(b *testing.B) {
-	// TODO: Реалізуйте інтеграційний бенчмарк для балансувальникка.
+func (s *IntegrationSuite) BenchmarkBalancer(c *C) {
+	if _, exists := os.LookupEnv("INTEGRATION_TEST"); !exists {
+		c.Skip("Integration test is not enabled")
+	}
+
+	for i := 0; i < c.N; i++ {
+		_, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
+		if err != nil {
+			c.Error(err)
+		}
+	}
 }
