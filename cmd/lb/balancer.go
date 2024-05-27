2b2308ad9969e56e09e04908e2382e87c9c8eb5d
Tytianiuk Artem <114434993+tytianiuk@users.noreply.github.com>
2024-05-27 08:42:43 +0300

    Add integration test (#2)
    * Rename modules
    
    * Add integration test

--- cmd/lb/balancer.go
+++ cmd/lb/balancer.go
@@ -12,8 +12,8 @@ import (
 	"sync"
 	"time"
 
-	"github.com/roman-mazur/architecture-practice-4-template/httptools"
-	"github.com/roman-mazur/architecture-practice-4-template/signal"
+	"github.com/merrymike-noname/architecture-practice-4/httptools"
+	"github.com/merrymike-noname/architecture-practice-4/signal"
 )
 
 var (
--- integration/balancer_test.go
+++ integration/balancer_test.go
@@ -19,14 +19,49 @@ func TestBalancer(t *testing.T) {
 		t.Skip("Integration test is not enabled")
 	}
 
-	// TODO: Реалізуйте інтеграційний тест для балансувальникка.
-	resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
-	if err != nil {
-		t.Error(err)
+	testCases := []struct {
+		endpoint string
+		expected int
+	}{
+		{"/api/v1/some-data", 0},
+		{"/api/v1/some-data2", 1},
+		{"/api/v1/some-data", 2},
 	}
-	t.Logf("response from [%s]", resp.Header.Get("lb-from"))
+
+	servers := make([]string, len(testCases))
+
+	for i, tc := range testCases {
+		url := fmt.Sprintf("%s%s", baseAddress, tc.endpoint)
+		resp, err := client.Get(url)
+		if err != nil {
+			t.Fatalf("Request to %s failed: %v", url, err)
+		}
+		defer resp.Body.Close()
+
+		server := resp.Header.Get("lb-from")
+		if server == "" {
+			t.Fatalf("Missing 'lb-from' header in response for request %d", i)
+		}
+		servers[i] = server
+	}
+
+	if servers[0] != servers[2] {
+		t.Errorf("Requests to the same endpoint were routed to different servers: got %s and %s", servers[0], servers[2])
+	}
 }
 
 func BenchmarkBalancer(b *testing.B) {
-	// TODO: Реалізуйте інтеграційний бенчмарк для балансувальникка.
+	if os.Getenv("INTEGRATION_TEST") == "" {
+		b.Skip("Integration test is not enabled")
+	}
+
+	url := fmt.Sprintf("%s/api/v1/some-data", baseAddress)
+
+	for i := 0; i < b.N; i++ {
+		resp, err := client.Get(url)
+		if err != nil {
+			b.Fatalf("Request to %s failed: %v", url, err)
+		}
+		defer resp.Body.Close()
+	}
 }
