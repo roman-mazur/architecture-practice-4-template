5b5c79882851b3b3e1fcf747a615ace40a7cbdc3
Maksym Diachok <maksdyachok2005@gmail.com>
2024-06-01 13:33:17 +0300

    Add basic test realization this test needs testing also we should implement Db.segments field and size arg to NewDb

--- design-db-practice-master/datastore/db_test.go
+++ design-db-practice-master/datastore/db_test.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"path/filepath"
 	"testing"
+	"time"
 )
 
 func TestDb_Put(t *testing.T) {
@@ -89,4 +90,74 @@ func TestDb_Put(t *testing.T) {
 		}
 	})
 
+}
+
+func TestDb_Segments_Merge(t *testing.T) {
+	saveDirectory, err := ioutil.TempDir("", "testDir")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(saveDirectory)
+
+	db, err := NewDb(saveDirectory, 45) // size as 2nd argument
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer db.Close()
+
+	t.Run("check creation of new segment", func(t *testing.T) {
+		db.Put("key1", "v1")
+		db.Put("key2", "v2")
+		db.Put("key3", "v3")
+		db.Put("key1", "V1")
+		db.Put("key4", "v4")
+		actual := len(db.segments)
+		expected := 2
+		if actual != expected {
+			t.Errorf("Expected %d files, but received %d.", expected, actual)
+		}
+	})
+
+	t.Run("check starting segmentation", func(t *testing.T) {
+		db.Put("key2", "V2")
+		actual := len(db.segments)
+		expected := 3
+		if actual != expected {
+			t.Errorf("Expected %d files before merge, but received %d.", expected, actual)
+		}
+
+		time.Sleep(2 * time.Second)
+
+		actualAfterMerge := len(db.segments)
+		expectedAfterMerge := 2
+		if actual != expected {
+			t.Errorf("Expected %d files after merge, but received %d.", expectedAfterMerge, actualAfterMerge)
+		}
+	})
+
+	t.Run("check not storing new values of duplicate keys", func(t *testing.T) {
+		actual, err := db.Get("key2")
+		if err != nil {
+			t.Error(err)
+		}
+		expected := "V2"
+		if actual != expected {
+			t.Errorf("An error occurred during segmentation. Expected value: %s, Actual one: %s", expected, actual)
+		}
+	})
+
+	t.Run("check size", func(t *testing.T) {
+		file, err := os.Open(db.segments[0].filePath)
+		defer file.Close()
+		if err != nil {
+			t.Error(err)
+		}
+		
+		inf, _ := file.Stat()
+		actual := inf.Size()
+		expected := int64(45)
+		if actual != expected {
+			t.Errorf("An error occurred during segmentation. Expected size %d, Actual one: %d", expected, actual)
+		}
+	})
 }
