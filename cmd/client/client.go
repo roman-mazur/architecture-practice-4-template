787dca2cbc910807621d62991572774cdb4da384
stasyukhymenko <yukhymenko.stas@gmail.com>
2024-06-02 20:55:58 +0300

    Change client.go

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -12,15 +12,28 @@ var target = flag.String("target", "http://localhost:8090", "request target")
 
 func main() {
 	flag.Parse()
-	client := new(http.Client)
-	client.Timeout = 10 * time.Second
+	client := &http.Client{Timeout: 10 * time.Second}
+
+	endpoints := []string{
+		"/api/v1/server1",
+		"/api/v2/server2",
+		"/api/v3/server3",
+	}
 
 	for range time.Tick(1 * time.Second) {
-		resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", *target))
-		if err == nil {
-			log.Printf("response %d", resp.StatusCode)
-		} else {
-			log.Printf("error %s", err)
+		for _, endpoint := range endpoints {
+			resp, err := client.Get(fmt.Sprintf("%s%s", *target, endpoint))
+			if err == nil {
+				log.Printf("response from %s: %d", endpoint, resp.StatusCode)
+			} else {
+				log.Printf("error from %s: %s", endpoint, err)
+			}
+			if resp != nil {
+				err := resp.Body.Close()
+				if err != nil {
+					return
+				}
+			}
 		}
 	}
 }
