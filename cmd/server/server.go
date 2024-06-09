1210e0a1ab7b70f423391be4a87be81cd04f2699
Tytianiuk Artem <tytianiuk@gmail.com>
2024-06-09 16:14:14 +0300

    Update cmd/server/server.go

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -16,9 +16,21 @@ var port = flag.Int("port", 8080, "server port")
 
 const confResponseDelaySec = "CONF_RESPONSE_DELAY_SEC"
 const confHealthFailure = "CONF_HEALTH_FAILURE"
+const url = "http://db:8083/db"
+
+type ResponseBody struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
+
+type RequestBody struct {
+	Value string `json:"value"`
+}
 
 func main() {
 	h := new(http.ServeMux)
+
+	client := http.DefaultClient
 
 	h.HandleFunc("/health", func(rw http.ResponseWriter, r *http.Request) {
 		rw.Header().Set("content-type", "text/plain")
@@ -32,6 +44,43 @@ func main() {
 	})
 
 	report := make(Report)
+
+	h.HandleFunc("/api/v1/some-data", func(rw http.ResponseWriter, r *http.Request) {
+		key := r.URL.Query().Get("key")
+		if key == "" {
+			rw.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		resp, err := client.Get(fmt.Sprintf("%s/%s", url, key))
+		if err != nil {
+			rw.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		statusOk := resp.StatusCode >= 200 && resp.StatusCode < 300
+
+		if !statusOk {
+			rw.WriteHeader(resp.StatusCode)
+			return
+		}
+
+		respDelayString := os.Getenv(confResponseDelaySec)
+		if delaySec, parseErr := strconv.Atoi(respDelayString); parseErr == nil && delaySec > 0 && delaySec < 300 {
+			time.Sleep(time.Duration(delaySec) * time.Second)
+		}
+
+		report.Process(r)
+
+		var responseBody ResponseBody
+		json.NewDecoder(resp.Body).Decode(&responseBody)
+
+		rw.Header().Set("content-type", "application/json")
+		rw.WriteHeader(http.StatusOK)
+		_ = json.NewEncoder(rw).Encode(responseBody)
+
+		defer resp.Body.Close()
+	})
 
 	h.HandleFunc("/api/v1/some-data1", func(rw http.ResponseWriter, r *http.Request) {
 		respDelayString := os.Getenv(confResponseDelaySec)
@@ -82,5 +131,13 @@ func main() {
 
 	server := httptools.CreateServer(*port, h)
 	server.Start()
+
+	buff := new(bytes.Buffer)
+	body := Request{Value: time.Now().Format(time.RFC3339)}
+	json.NewEncoder(buff).Encode(body)
+
+	res, _ := client.Post(fmt.Sprintf("%s/merrymike-noname", url), "application/json", buff)
+	defer res.Body.Close()
+
 	signal.WaitForTerminationSignal()
 }
