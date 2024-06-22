51e7d9806a16312a40b6c5be59947fcf5835eab5
Bogdan Lepkov <122379140+nevrodegenerate@users.noreply.github.com>
2024-06-22 11:21:38 +0300

    Added balancer tests

--- integration/balancer_test.go
+++ integration/balancer_test.go
@@ -14,19 +14,57 @@ var client = http.Client{
 	Timeout: 3 * time.Second,
 }
 
-func TestBalancer(t *testing.T) {
-	if _, exists := os.LookupEnv("INTEGRATION_TEST"); !exists {
-		t.Skip("Integration test is not enabled")
-	}
+func (s *IntegrationSuite) TestBalancer() {
+  if _, exists := os.LookupEnv("INTEGRATION_TEST"); !exists {
+    s.T().Skip("Integration test is not enabled")
+  }
 
-	// TODO: Реалізуйте інтеграційний тест для балансувальникка.
-	resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
-	if err != nil {
-		t.Error(err)
-	}
-	t.Logf("response from [%s]", resp.Header.Get("lb-from"))
+  server1, err := client.Get(fmt.Sprintf("%s/check", baseAddress))
+  if err != nil {
+    s.T().Error(err)
+  }
+
+  server1Header := server1.Header.Get("lb-from")
+  assert.Equal(s.T(), "server1:8080", server1Header)
+
+  server2, err := client.Get(fmt.Sprintf("%s/check4", baseAddress))
+  if err != nil {
+    s.T().Error(err)
+  }
+
+  server2Header := server2.Header.Get("lb-from")
+  assert.Equal(s.T(), "server2:8080", server2Header)
+
+  server3, err := client.Get(fmt.Sprintf("%s/check2", baseAddress))
+  if err != nil {
+    s.T().Error(err)
+  }
+
+  server3Header := server3.Header.Get("lb-from")
+  assert.Equal(s.T(), "server3:8080", server3Header)
+
+  server1Repeat, err := client.Get(fmt.Sprintf("%s/check", baseAddress))
+  if err != nil {
+    s.T().Error(err)
+  }
+
+  server1RepeatHeader := server1Repeat.Header.Get("lb-from")
+  assert.Equal(s.T(), server1Header, server1RepeatHeader)
 }
 
-func BenchmarkBalancer(b *testing.B) {
-	// TODO: Реалізуйте інтеграційний бенчмарк для балансувальникка.
+func (s *IntegrationSuite) BenchmarkBalancer(b *testing.B) {
+  if _, exists := os.LookupEnv("INTEGRATION_TEST"); !exists {
+    b.Skip("Integration test is not enabled")
+  }
+
+  for i := 0; i < b.N; i++ {
+    _, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
+    if err != nil {
+      b.Error(err)
+    }
+  }
+}
+
+func TestIntegrationSuite(t *testing.T) {
+  suite.Run(t, new(IntegrationSuite))
 }
