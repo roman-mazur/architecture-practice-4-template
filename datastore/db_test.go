5200a0edfa2e453bf84699ae2ade174c699d5c8b
Bilyi Bohdan <114570417+TarnishedGhost@users.noreply.github.com>
2024-06-22 16:11:36 +0300

    Create db_test.go

---
+++ datastore/db_test.go
@@ -0,0 +1,164 @@
+package datastore
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+)
+
+func TestDb_Put(t *testing.T) {
+	dir, err := os.MkdirTemp("", "test-db")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+
+	db, err := NewDb(dir, 45)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer db.Close()
+
+	pairs := [][]string{
+		{"1", "v1"},
+		{"2", "v2"},
+		{"3", "v3"},
+	}
+
+	t.Run("put/get", func(t *testing.T) {
+		for _, pair := range pairs {
+			err := db.Put(pair[0], pair[1])
+			if err != nil {
+				t.Errorf("Cannot put %s: %s", pair[0], err)
+			}
+			value, err := db.Get(pair[0])
+			if err != nil {
+				t.Errorf("Cannot get %s: %s", pair[0], err)
+			}
+			if value != pair[1] {
+				t.Errorf("Bad value returned expected %s, got %s", pair[1], value)
+			}
+		}
+	})
+
+	outFile, err := os.Open(filepath.Join(dir, outFileName+"0"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer outFile.Close()
+
+	outInfo, err := outFile.Stat()
+	if err != nil {
+		t.Fatal(err)
+	}
+	size1 := outInfo.Size()
+
+	t.Run("file growth", func(t *testing.T) {
+		for _, pair := range pairs {
+			err := db.Put(pair[0], pair[1])
+			if err != nil {
+				t.Errorf("Cannot put %s: %s", pair[0], err)
+			}
+		}
+		outFile.Close()
+		outFile, err = os.Open(filepath.Join(dir, outFileName+"0"))
+		if err != nil {
+			t.Fatal(err)
+		}
+		outInfo, err := outFile.Stat()
+		if err != nil {
+			t.Fatal(err)
+		}
+		if size1 != outInfo.Size() {
+			t.Errorf("Unexpected size (%d vs %d)", size1, outInfo.Size())
+		}
+	})
+
+	t.Run("new db process", func(t *testing.T) {
+		if err := db.Close(); err != nil {
+			t.Fatal(err)
+		}
+		db, err = NewDb(dir, 45)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer db.Close()
+
+		for _, pair := range pairs {
+			value, err := db.Get(pair[0])
+			if err != nil {
+				t.Errorf("Cannot get %s: %s", pair[0], err)
+			}
+			if value != pair[1] {
+				t.Errorf("Bad value returned expected %s, got %s", pair[1], value)
+			}
+		}
+	})
+}
+
+func TestDb_Segmentation(t *testing.T) {
+	saveDirectory, err := os.MkdirTemp("", "testDir")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(saveDirectory)
+
+	db, err := NewDb(saveDirectory, 35)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer db.Close()
+
+	t.Run("check creation of new file", func(t *testing.T) {
+		db.Put("1", "v1")
+		db.Put("2", "v2")
+		db.Put("3", "v3")
+		db.Put("2", "v5")
+		actualTwoFiles := len(db.segmentManager.segments)
+		expected2Files := 2
+		if actualTwoFiles != expected2Files {
+			t.Errorf("An error occurred during segmentation. Expected 2 files, but received %d.", len(db.segmentManager.segments))
+		}
+	})
+
+	t.Run("check starting segmentation", func(t *testing.T) {
+		db.Put("4", "v4")
+		actualThreeFiles := len(db.segmentManager.segments)
+		expected3Files := 3
+		if actualThreeFiles != expected3Files {
+			t.Errorf("An error occurred during segmentation. Expected 3 files, but received %d.", len(db.segmentManager.segments))
+		}
+
+		time.Sleep(2 * time.Second)
+
+		actualTwoFiles := len(db.segmentManager.segments)
+		expected2Files := 2
+		if actualTwoFiles != expected2Files {
+			t.Errorf("An error occurred during segmentation. Expected 2 files, but received %d.", len(db.segmentManager.segments))
+		}
+	})
+
+	t.Run("check not storing new values of duplicate keys", func(t *testing.T) {
+		actual, _ := db.Get("2")
+		expected := "v5"
+		if actual != expected {
+			t.Errorf("An error occurred during segmentation. Expected value: %s, Actual one: %s", expected, actual)
+		}
+	})
+
+	t.Run("check size", func(t *testing.T) {
+		file, err := os.Open(db.segmentManager.segments[0].filePath)
+		if err != nil {
+			t.Error(err)
+		}
+
+		defer file.Close()
+		inf, _ := file.Stat()
+		actual := inf.Size()
+		expected := int64(45)
+		if actual != expected {
+			t.Errorf("An error occurred during segmentation. Expected size %d, Actual one: %d", expected, actual)
+		}
+	})
+}
